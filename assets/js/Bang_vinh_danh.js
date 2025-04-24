5cbc3df4bc6f5f54aa2036313b8674892332cc98
VIKI_LAPTOP\kim29 <kim29042004@gmail.com>
2025-04-24 23:21:00 +0700

    Cứu Bảo done

--- assets/js/Bang_vinh_danh.js
+++ assets/js/Bang_vinh_danh.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Load header
-    fetch('../../ModelViews/Header_dự_án/Header_dự_án.html')
+    fetch('./Header_dự_án/Header_dự_án.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('header-container').innerHTML = data;
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     
     // Load footer
-    fetch('../../ModelViews/Footer/footer.html')
+    fetch('./Footer/footer.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('footer-container').innerHTML = data;
